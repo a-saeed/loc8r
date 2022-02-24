c98ce915c934a0e39aa189f17e8d664b34be046a
a-saeed <31377230+a-saeed@users.noreply.github.com>
2022-02-24 02:31:30 +0200

    adding a controller for location functionalities

---
+++ app_server/controllers/locations.js
@@ -0,0 +1,15 @@
+
+/**GET 'Home' page === list of all available nearby locations with wifi*/
+export const homeList = (req, res, next) => {
+  res.render('index', { title: 'Express' });
+}
+
+/**GET 'LocationInfo' page */
+export const locationInfo = (req, res, next) => {
+  res.render('index', { title: 'Location info' });
+}
+
+/**GET 'AddReview' page */
+export const addReview = (req, res, next) => {
+  res.render('index', { title: 'add a review' });
+}
