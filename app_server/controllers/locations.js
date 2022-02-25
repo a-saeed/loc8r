5e718e0303808d2d4025a035d344a8790228490d
a-saeed <31377230+a-saeed@users.noreply.github.com>
2022-02-25 14:32:26 +0200

    move homepage data from view to controller

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,7 +1,34 @@
 
 /**GET 'Home' page === list of all available nearby locations with wifi*/
 export const homeList = (req, res, next) => {
-  res.render('locations_list', { title: 'Express' });
+  res.render('locations_list', {
+    title: 'loc8r - Find places to work with wifi near you!',
+    pageHeader: {  //page header object
+      title: 'loc8r',
+      strapLine: 'Find places to work with with wifi near you!'
+    },
+    locations: [   //array of locations objects
+      {
+      name: 'StarCups',
+      address: '125 High Street, Reading, RG6 1PS',
+      rating: '3',
+      facilities: ['Hot drinks', 'Food', 'Premium Wifi'],
+      distance: '5000m'},
+      {
+      name: 'cafeHero',
+      address: '125 High Street, Reading, RG6 1PS',
+      rating: '4',
+      facilities: ['Hot drinks', 'Food', 'Premium Wifi'],
+      distance: '200m'},
+      {
+      name: 'Burger Queen',
+      address: '125 High Street, Reading, RG6 1PS',
+      rating: '2',
+      facilities: ['Hot drinks', 'Premium Wifi'],
+      distance: '250m'
+      }
+    ] 
+  });
 }
 
 /**GET 'LocationInfo' page */
