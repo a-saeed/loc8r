218249427bc5d4aa2247d2268c374eba0a4d9427
a-saeed <31377230+a-saeed@users.noreply.github.com>
2022-03-07 21:41:53 +0200

    return a list of near real nearby avail locations

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -1,7 +1,49 @@
 import CustomError from "../models/CustomError.js"
 import locationModel from "../models/locationsModel.js"
 
-const locationsListByDistance = async(req, res, next) => {
+const locationsListByDistance = async (req, res, next) => {
+    try {
+        //read lng and lat from query strings
+        const longitude = parseFloat(req.query.longitude)
+        const latitude = parseFloat(req.query.latitude)
+        const near = { //create geoJSON point
+            type: "point",
+            coordinates: [longitude, latitude]
+        };
+        const geoOptions = {
+            distanceField: "distance.calculated",
+            key: "coords",
+            spherical: "true",
+            maxDistance: 20000, //20km
+        };
+        if (!longitude || !latitude) {
+            return res.status(404).json({ message: "lng and lat query strings are required" })
+        }
+        const results = await locationModel.aggregate([
+            {
+                $geoNear: {
+                    near,
+                    ...geoOptions
+                }
+            },
+            {
+                $limit: 10 //return no more than 10 near locations
+            }
+        ])
+        const locations = results.map(result => { //output all near locations
+            return {
+                id: result._id,
+                name: result.name,
+                address: result.address,
+                rating: result.rating,
+                facilities: result.facilities,
+                distance: `${result.distance.calculated.toFixed()}m`
+            }   
+        })
+        res.status(200).json(locations)
+    } catch (error) {
+        next(new CustomError(404, "an error occurred " + error))
+    }
     
 }
 
