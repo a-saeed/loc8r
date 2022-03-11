5df2b358e0d60a2b54389ed50cb300b07f10c28f
a-saeed <31377230+a-saeed@users.noreply.github.com>
2022-03-11 17:47:21 +0200

    update an existing location

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -94,7 +94,43 @@ const locationsReadOne = async (req, res, next) => {
 }
 
 const locationsUpdateOne = async(req, res, next) => {
-    
+    try {
+        if (!req.params.locationId)
+            res.status(404).json("Not found, location ID is required")
+        
+        const location = await locationModel.findById(req.params.locationId).select('-reviews -rating') //return doc except reviews and rating
+        if (!location) {
+            return res.status(404).json({message: "no location found"})
+        }
+        //update location based on req.body
+        location.name = req.body.name,
+        location.address = req.body.address,
+        location.facilities = req.body.facilities.split(","),
+        location.coords = {
+            type: "Point",
+            coordinates: [
+                parseFloat(req.body.lng),
+                parseFloat(req.body.lat)
+            ]
+        },
+        location.openingTimes = [{
+            days: req.body.days1,
+            opening: req.body.opening1,
+            closing: req.body.closing1,
+            closed: req.body.closed1
+        },
+        {
+            days: req.body.days2,
+            opening: req.body.opening2,
+            closing: req.body.closing2,
+            closed: req.body.closed2
+                }]
+        await location.save()
+        res.status(200).json(location)
+
+    } catch (error) {
+        next(new CustomError(404, "an error occurred " + error))
+    }
 }
 
 const locationsDeleteOne = async(req, res, next) => {
