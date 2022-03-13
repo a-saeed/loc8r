fe833580252cd56942799c0b240a7e778b6bcd9d
a-saeed <31377230+a-saeed@users.noreply.github.com>
2022-03-14 00:08:00 +0200

    replace location hardcoded data with data from api

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -41,52 +41,25 @@ export const homeList = (req, res, next) => {
 
 /**GET 'LocationInfo' page */
 export const locationInfo = (req, res, next) => {
-  res.render('location_info', {
-    pageHeader: { title: 'starCups' },
-    sideBar: {
-      context: ' is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.',
-      callToAction: 'please leave a review'
-    },
-    location: {
-      name: 'starCups',
-      address: ' 125 High Street, Reading, RG6 1PS',
-      rating: '3',
-      facilities: ['HOt drinks', 'Food', 'Premium Wifi'],
-      coords: { lat: 51.455041, lng: -0.9690884 },
-      openingTimes: [
-        {
-          days: 'monday - friday ',
-          opening: '7:00am',
-          closing: '7:00pm',
-          closed: false
-        },
-        {
-           days: 'saturday ',
-          opening: '8:00am',
-          closing: '5:00pm',
-          closed: false
-        },
-        {
-          days: 'sunday ',
-          closed: 'true'
-        }
-      ],
-      reviews: [
-        {
-          author: 'Simon Holmes',
-          rating: '5',
-          timestamp: '16 June 2013',
-          reviewText: 'What a great place.'
-        },
-        {
-          author: 'Charlie Chaplin',
-          rating: '3',
-          timestamp: '16 June 2013',
-          reviewText: 'Coffee was not great .'
-        }
-      ]
-    }
-  });
+  const path = `/api/locations/${req.params.locationId}`
+  const requestOptions = {
+    url: `${apiOptions.server}${path}`,
+    method: 'get'
+  }
+
+  axios(requestOptions)
+    .then(response => {
+      const data = response.data
+      data.coords = {
+        longitude: data.coords.coordinates[0],
+        latitude: data.coords.coordinates[1]
+      }
+      renderDetailPage(req, res, data)
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  
 }
 
 /**GET 'AddReview' page */
@@ -98,7 +71,6 @@ export const addReview = (req, res, next) => {
     }
   );
 }
-
 /* --------------------------------- HELPERS -------------------------------- */
 /**
  * Decoupling the rendering from the application logic
@@ -135,5 +107,15 @@ const renderHomepage = (req, res, responseBody) => {
     },
     locations: responseBody,
     message
+  });
+}
+const renderDetailPage = (req, res, location) => {
+  res.render('location_info', {
+    pageHeader: { title: location.name },
+    sideBar: {
+      context: ' is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.',
+      callToAction: 'please leave a review'
+    },
+    location
   });
 }
